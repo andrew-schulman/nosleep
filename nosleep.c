39c9116cf29dec2424dec29ce976b58685b6e67c
Andrew Schulman <schulman.andrew@epa.gov>
2011-09-22 14:53:13 -0400

    fix --version option

--- nosleep.c
+++ nosleep.c
@@ -22,31 +22,28 @@
 #include <windows.h>
 
 /* Program docs */
-const char *argp_program_version = "nosleep v1.0 | Copyright 2011 Andrew E. Schulman";
-const char *argp_program_bug_address = "the Cygwin mailing list <cygwin@cygwin.com>";
-static char doc[] = "Run a command while inhibiting computer sleep or hibernation.";
-static char args_doc[] = "command [args]";	/* non-option arguments */
+static char version[]     = "nosleep v1.0 | Copyright 2011 Andrew E. Schulman";
+static char bug_address[] = "the Cygwin mailing list <cygwin@cygwin.com>";
+static char doc[]         = "Run a command while inhibiting computer sleep or hibernation.";
+static char args_doc[]    = "command [args]";	/* non-option arguments */
 
-/* Options */
+/* Program options */
 static struct argp_option options[] = {
-  {"awaymode",  'a', 0, 0,  "Substitute away mode for sleep mode" },
+  {"awaymode",  'a', 0, 0,  "Use away mode instead of sleep mode" },
   {"display",   'd', 0, 0,  "Keep the display on" },
-  {"powerplan", 'p', 0, 0,  "Manipulate the active power plan" },
-  { 0 }
+  {"powerplan", 'p', 0, 0,  "Adjust the power plan" },
+  { 0, 0, 0, 0, 0 }
 };
      
-/* Argument structure, for communication from parse_opt to main */
-struct arguments
+/* Options structure, for passing options around */
+struct program_options
 {
-  char *args[2];                /* arg1 & arg2 */
-  int silent, verbose;
-  char *output_file;
+  int awaymode, display, powerplan;
 };
 
 /* argp parser */
 static error_t parse_opt(int, char *, struct argp_state *);
 static struct argp argp = {options, parse_opt, args_doc, doc};
-
 
 /*
  * main()
@@ -55,10 +52,11 @@ static struct argp argp = {options, parse_opt, args_doc, doc};
 int
 main(int argc, char *argv[]) {
 
+  argp_program_version = version;
+  argp_program_bug_address = bug_address;
   argp_parse(&argp, argc, argv, 0, 0, 0);
 
   int execution_state = ES_CONTINUOUS | ES_SYSTEM_REQUIRED | ES_DISPLAY_REQUIRED;
-  pid_t child;
 
   /* Inhibit sleep/hibernation */
   if (! SetThreadExecutionState(execution_state)) {
@@ -67,16 +65,17 @@ main(int argc, char *argv[]) {
   }
   
   /* Start child process */
-  child = fork();
-  if (child == 0)
+  switch (fork()) {
+  case 0:	/* child */
     execvp(argv[1], argv+1);
-  if (child == -1) {
+    exit(EXIT_FAILURE);
+  case -1:	/* parent (error) */
     fprintf(stderr, "%s: Error: failed to create child process", argv[0]);
     exit(EXIT_FAILURE);
   }
 
   /* Wait for child to exit */
-  waitpid(child, 0, 0);
+  wait(0);
 
   /* Restore power options if necessary */
 
@@ -90,23 +89,20 @@ main(int argc, char *argv[]) {
 static error_t
 parse_opt (int key, char *arg, struct argp_state *state)
 {
-  struct arguments *arguments = state->input;
+  struct program_options *arguments = state->input;
 
   switch (key) {
-    case 'q':
-    case 's':
-      arguments->silent = 1;
+    case 'a':
+      arguments->awaymode = 1;
       break;
-    case 'v':
-      arguments->verbose = 1;
+    case 'd':
+      arguments->display = 1;
       break;
-    case 'o':
-      arguments->output_file = arg;
+    case 'p':
+      arguments->powerplan = 1;
       break;
 
     case ARGP_KEY_ARG:
-      arguments->args[state->arg_num] = arg;
-
       break;
 
     case ARGP_KEY_END:
